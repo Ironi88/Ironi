d7da3d28942791bbcda9e6f9e19f256c8702cedc
Ironi88 <100360810+Ironi88@users.noreply.github.com>
2022-05-12 14:21:47 +0200

    create readme

--- main.py
+++ main.py
@@ -23,29 +23,20 @@ def formular():
         datum = data["datum"]
         gefahrene_Km = data["gefahrene Km"]
         gefahrene_Hm = data["gefahrene Hm"]
-        my_dict = [{"Vorname": vorname, "Nachname": nachname, "datum": datum, "gefahrene Km": gefahrene_Km, "gefahrene Hm": gefahrene_Hm}]
+        try:
+            with open("aktivitaeten_2.json", "r") as open_file:
+                datei_inhalt = json.load(open_file)
+        except FileNotFoundError:
+            datei_inhalt = []
+
+        my_dict = {"Vorname": vorname, "Nachname": nachname, "datum": datum, "gefahrene Km": gefahrene_Km, "gefahrene Hm": gefahrene_Hm}
+        datei_inhalt.append(my_dict)
+
         with open("aktivitaeten_2.json", "w") as open_file:
-            json.dump(my_dict, open_file, indent=4)  #ident=4 dient dazu, um JSON File "schöner" anzuzeigen.
+            json.dump(datei_inhalt, open_file, indent=4)  #ident=4 dient dazu, um JSON File "schöner" anzuzeigen.
         return render_template("formular.html")
     else:
         return render_template("formular.html")
-def load_data_json(pfad, standard_wert = []):
-    #Quelle: https://stackabuse.com/reading-and-writing-json-to-a-file-in-python/ & https://www.programiz.com/python-programming/json
-    #das JSONFile wird im read.modus "r" geöffnet, "w" würde das gesamte File löschen.
-    try:
-        with open("aktivitaeten_2.json", "r") as open_file:
-            return json.load(open_file)
-    except Exception:
-        return aktivitaeten_2.json
-
-
-
-@app.route("/speichern/<aktivitaet>")
-def speichern(aktivitaet):
-    zeitpunkt, aktivitaet = daten.aktivitaet_speichern(aktivitaet)
-
-    return "Gespeichert: " + aktivitaet + " um " + str(zeitpunkt)
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
