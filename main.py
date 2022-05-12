1304df7130b6a14b1e18a2f728a582ea5ccc7edf
Ironi88 <100360810+Ironi88@users.noreply.github.com>
2022-05-12 16:09:04 +0200

    create readme

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-import daten
 import json
 
 app = Flask("Ironi")
@@ -14,9 +13,9 @@ def index():
     return render_template("index.html")
 
 #Verlinkung auf Formular
-@app.route("/formular/", methods=['GET', 'POST'])
+@app.route("/formular/", methods=["GET", "POST"])
 def formular():
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.form
         vorname = data["vorname"]
         nachname = data["nachname"]
@@ -27,16 +26,21 @@ def formular():
             with open("aktivitaeten_2.json", "r") as open_file:
                 datei_inhalt = json.load(open_file)
         except FileNotFoundError:
-            datei_inhalt = []
+            datei_inhalt = [] #wenn es noch keine Daten aufzeigt
 
         my_dict = {"Vorname": vorname, "Nachname": nachname, "datum": datum, "gefahrene Km": gefahrene_Km, "gefahrene Hm": gefahrene_Hm}
         datei_inhalt.append(my_dict)
 
         with open("aktivitaeten_2.json", "w") as open_file:
             json.dump(datei_inhalt, open_file, indent=4)  #ident=4 dient dazu, um JSON File "schöner" anzuzeigen.
-        return render_template("formular.html")
+        return str("deine Daten sind gespeichert") #Text nach dem ausfüllen vom Formular
     else:
         return render_template("formular.html")
+
+@app.route("/berechnung/", methods=["GET", "POST"])
+def berechnung():
+    return render_template("berechnung.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
