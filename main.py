2800fded85b7331ec85dc062da2052e13b7d7a9c
Ironi88 <100360810+Ironi88@users.noreply.github.com>
2022-05-17 14:37:20 +0200

    create readme

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def formular():
         gefahrene_Km = data["gefahrene Km"]
         gefahrene_Hm = data["gefahrene Hm"]
         try:
-            with open("aktivitaeten_2.json", "r") as open_file:
+            with open("aktivitaeten.json", "r") as open_file:
                 datei_inhalt = json.load(open_file)
         except FileNotFoundError:
             datei_inhalt = [] #wenn es noch keine Daten aufzeigt
@@ -31,15 +31,23 @@ def formular():
         my_dict = {"Vorname": vorname, "Nachname": nachname, "datum": datum, "gefahrene Km": gefahrene_Km, "gefahrene Hm": gefahrene_Hm}
         datei_inhalt.append(my_dict)
 
-        with open("aktivitaeten_2.json", "w") as open_file:
+        with open("aktivitaeten.json", "w") as open_file:
             json.dump(datei_inhalt, open_file, indent=4)  #ident=4 dient dazu, um JSON File "schöner" anzuzeigen.
         return str("deine Daten sind gespeichert") #Text nach dem ausfüllen vom Formular
     else:
         return render_template("formular.html")
 
 @app.route("/berechnung/", methods=["GET", "POST"])
-def berechnung():
-    return render_template("berechnung.html")
+def berechnung(vorname, km, hm):
+    vorname = vorname
+    gefahrene_km = gefahrene_km(float(km))
+    gefahrene_hm = gefahrene_hm(float(hm))
+
+    return "Mega cool du hast: " #+ str(ergebnis_steinboecke) "gesammelt"
+
+@app.route("/regeln/", methods=["GET", "POST"]) #Erklärt das Spiel
+def regeln():
+    return render_template("regeln.html")
 
 
 if __name__ == "__main__":
