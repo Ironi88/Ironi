48278103c2dfcb7fa294426bbf0dca51783a9c2d
Ironi88 <100360810+Ironi88@users.noreply.github.com>
2022-06-15 21:19:05 +0200

    create readme

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 # 🚴 ️VeloMio  🚴‍♀ <!-- Plug in Emoji Support einfügen  :emjoiname -->
 ☝ Mein erstes Python Programm  ☝ 
 
-## Inhaltsübersicht
+## Inhaltsübersicht 
 1. Problembeschreibung/Motivation
 2. Benutzung
 3. Architektur 
 4. Ungelöste Themen und Verbesserungsvorschläge 
 5. Link to github
+6. Quellen
 
-##1. Problembeschreibung/Motivation
+## 1. Problembeschreibung/Motivation
 Die Projektidee hängt sehr stark mit meinem Hobby Triathlon zusammen. 
 Das Programm beschränkt sich, aber auf eine der drei Disziplinen, dem Radfahren. 
 Beim Sport ist es extrem wichtig ein hohes Level an intrinsische Motivation zu haben. 
@@ -18,22 +19,22 @@ Eine Dokumentation und ein Ziel fördert die Motivation.
 Ein Ziel vor Augen treibt die Menschen an und es kommt Bewegung ins Spiel. 
 Das Spiel wurde für das Team von sportbenzin.ch programmiert. 
 
-##2. Betrieb 
-- Flask, Plottly und Json werden benötigt
+## 2. Betrieb 
+- Flask, Plottly, Bootstrap und Json werden benötigt
 - Main.py muss als erstes ausgeführt werden
+- Plugin "Yet another emoji support" für die emoji
 
 
-##3. Benutzung
+## 3. Benutzung
 Erfassen: Vorname, Datum und Kilometer
 Verarbeitung: speichern Datenbank, Filter Funktion für die oben genannten Values 
 Punkte System der pro 100k gibt es ein Steinbock. 
 
 Ausgabe: Gesammelte KM zeigen die gesammelte Anzahl der Steinbock an
 
-##3.👀 Architektur 
+## 3.👀 Architektur 
 <!-- Erstellt im dawio 
 https://app.diagrams.net/#G1lI50nTNPo-A5iqcfEOgooin0duN972aB -->
-
 ![Ablauf Grafisch](Diagramm.png)
 ##4.Ungelöste/unbearbeitete Probleme
 Was wurde nicht gelöst?
@@ -44,9 +45,18 @@ Der Code würde sonst unübersichtlich und Änderungen sind mühsam vorzunehmen.
 Welche Verbesserungen könnten noch gemacht werden?
 - Weitere Optionen hinzufügen um Steinboecke zu sammeln
 - Challenge Ende definieren
+- Unvollstaendige Eingabe im Formular wir abgespeichert. Pflichtfelder
+  fehlen
 
-##5. Link to github
+## 5. Link to github
 Link to my code https://github.com/Ironi88/Ironi
+
+## 6. Quellen
+- Tutoring 
+- Foliensaetze aus dem Modulunterricht
+- https://getbootstrap.com/docs/4.5/getting-started/introduction/
+- google
+
 
 
 
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 #Import von Modulen
-from flask import Flask
-from flask import render_template
-from flask import request
-import json
-from json import loads
+from flask import Flask  #architektur/ Struktur
+from flask import render_template #ausgabe,rueckgabe
+from flask import request #forumal get und post
+import json #java datenbank
+from json import loads #gespeicherte Daten laden
 
 
-import plotly.express as px
-from plotly.offline import plot
+import plotly.express as px #Datenvisualisierungsbibliothek
+from plotly.offline import plot #offline plot funktion als div erstellt und dann mit flask rendern
 
-#mein erstes Projekt in python :)
-app = Flask("Ironi")
 
-# Funktion zum die Daten gemachten Eintraege zu laden. Wir mehrmals gebracht.
-# Um  den Code so kurz wie möglich zu halten kriegt sie später eine kurze def data = get_data()
+app = Flask("Ironi") #mein erstes Projekt in python :)
+
+# Funktion zum die gemachten Eintraege zu laden. Wir mehrmals gebracht.
+# Um  den Code so kurz wie möglich zu halten. spaeter data = get_data()
 def get_data():
     try:
         with open("aktivitaeten.json", "r") as open_file:
             eintraege = json.load(open_file)
     except FileNotFoundError:
-        eintraege = [] #für den Fall das es der erste Eintrag ist.
+        eintraege = [] #für den Fall das es der erste Eintrag ist. Sonst gaebe es Error
     return eintraege
 
 @app.route("/")
@@ -28,24 +28,24 @@ def index():
 
 
 #Verlinkung auf Formular
-@app.route("/formular/", methods=["GET", "POST"])
+@app.route("/formular/", methods=["GET", "POST"])  #Zusatz zur URL / akzeptierte Methoden
 def formular():
-    if request.method == "POST":
+    if request.method == "POST":  #if Bedingung Eintrag #Post Datenuebermittlung
         data = request.form
         vorname = data["vorname"]
         datum = data["datum"]
         gefahrene_Km = data["gefahrene Km"]
 
 
-        eintraege = get_data() #def siehe oben nicht mehrmals daten holen.. refectering..
+        eintraege = get_data() #def siehe oben nicht mehrmals daten holen..
         eingabe_formular = {"Vorname": vorname,"datum": datum, "gefahrene Km": gefahrene_Km}
-        eintraege.append(eingabe_formular)
+        eintraege.append(eingabe_formular) #Listen mit neuer Eingabe ergaenzen
 
         with open("aktivitaeten.json", "w") as open_file:
             json.dump(eintraege, open_file, indent=4)  #ident=4 dient dazu, um JSON File "schöner" anzuzeigen.
             text = "Deine Daten wurden gespeichert" #Anzeigetext nachdem die Daten eingeben und gesendet sind
-        return render_template("formular.html", anzeige=text) #anzeige wird im html benötig
-    else:
+        return render_template("formular.html", anzeige=text) #anzeige wird im html benötig Formular benoetigt
+    else:#Felder werden geleert und eine neue Eingabe ist möglich
         return render_template("formular.html")
 
 @app.route("/berechnung/", methods=["GET", "POST"])
@@ -53,7 +53,7 @@ def formular():
 #Berechung der Anzahlsteinboecke Steinboecke
 def berechnung():
     data = get_data()   # defintion von oben Zugriff auf json datei aktivitäten
-    steinbock_remo = 0  #0 verhindert Errow bei Datensatz 0
+    steinbock_remo = 0  # verhindert Error bei Datensatz 0
     summe_km_remo = 0
     steinbock_rahel=0
     summe_km_rahel =0
@@ -113,7 +113,7 @@ def berechnung():
         y=[steinbock_remo, steinbock_rahel, steinbock_moni, steinbock_vroni],
         labels={"x": "Name Sportbenzin MitarbeiterIn", "y": "Anzahl Steinboecke"},
     )
-    div_visual = plot(visual, output_type="div")
+    div_visual = plot(visual, output_type="div")  #Balkendiagramm import Plotly ist noetig fuer diesen Schritt
 
 
     return render_template("berechnung.html",
@@ -126,9 +126,11 @@ def berechnung():
 @app.route("/datensammlung/", methods=["GET", "POST"])
 def datensammlung():
     with open("aktivitaeten.json") as open_file:
-        json_as_string = open_file.read()
+        json_as_string = open_file.read() #string ist noetig damit java und python kompatibel "werden"
         daten_inhalt = loads(json_as_string)
     return render_template("datensammlung.html", daten_inhalt=daten_inhalt)
 
+#noetig fuer die Ausfuehrung auf den "Server" (Exit code 0). Den technischen Hintergrund kann ich nicht erklaeren
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
